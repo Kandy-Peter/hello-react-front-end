5052637fe98c87b834fb0fb8aa64a39165c138dc
kandy-peter <kandypeter03@gmail.com>
2022-06-13 22:43:18 +0200

    Create and implemnet the front-end

---
+++ src/redux/greeting.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+const FETCH_GREETING_SUCCESS = 'FETCH_GREETING_SUCCESS';
+
+const getGreeting = (greeting) => ({
+  type: FETCH_GREETING_SUCCESS,
+  payload: greeting,
+});
+
+const greetingState = {
+  greeting: '',
+};
+
+export const reducer = (state = greetingState,
+  { type, payload }) => {
+    switch (type) {
+      case FETCH_GREETING_SUCCESS:
+        return {
+          ...state,
+          greeting: payload,
+        };
+      default:
+        return state;
+    };
+};
+
+const fetchGreetings = () => async (dispatch) => {
+  const result = await axios.get('http://127.0.0.1:3000/api/v1/greetings');
+    const data = result.data.greetings
+    dispatch(getGreeting(data));
+};
+
+export default fetchGreetings;
